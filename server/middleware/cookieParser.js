a57fb77ef5c09c96ac644c5524e0616e9fbe3aec
Rachel Quirbach <rachelquirbachy@gmail.com>
2023-01-26 22:56:49 -0500

    "Complete Parsing Cookie"

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,11 +1,16 @@
+const utils = require('../lib/hashUtils');
+const models = require('../models');
+
 //GLearn Goals
 // Write a middleware function that will:
 // (1) access the cookies on an incoming request,
 // (2) parse them into an object, and
 // (3) assign this object to a cookies property on the request.
 
-//Spec Runner Tests
+//Spec Runner Tests for Cookies
 //parses cookies and assigns an object of key-value pairs to a session property on the request
+
+//Spec Runner Tests for Sessions
 //initializes a new session when there are no cookies on the request
 //sets a new cookie on the response when a session is initialized
 //assigns a session object to the request if a session already exists
@@ -13,20 +18,44 @@
 //assigns a username and userId property to the session object if the session is assigned to a user
 //clears and reassigns a new cookie if there is no session assigned to the cookie
 const parseCookies = (req, res, next) => {
-  //console.log('THIS IS REQUEST', req);
-  // if (req.headers.cookie !== undefined) {
-  //   var string = '{' + req.headers.cookie.split(' ').join().replaceAll(';', '').replaceAll('=', ': ').replaceAll(',', ', ') + '}';
-  //   console.log('Parsing cookies: ', JSON.parse(string));
-  // }
 
   //pull cookies out of request
+  var cookies = req.headers.cookie;
 
-  //if cookies
-  //  parse into object
+  // if cookies, parse into object
+  if (cookies !== undefined) {
+    cookies = JSON.parse('{"' + cookies.split(' ').join('", "').replaceAll(';', '').replaceAll('=', '": "') + '"}');
+    //  assign object to cookies property on request (req._setCookiesVariable)
+    for (var key in cookies) {
+      req._setCookiesVariable(key, cookies[key]);
+    }
+  }
+
+  // Go to the next middleware fn
+  next();
+
+  // return (models.Sessions.create({ 'hash': null, 'userId': null }))
+  //   .then(results => {
+  //     return utils.createRandom32String();
+  //   })
+  //   .then(hash => {
+  //     return models.Sessions.update({ 'hash': hash, 'userId': null });
+  //   })
+  //   .then(results => {
+  //     res.status(201).send(results.hash);
+  //     next();
+  //   })
+  //   .error(error => {
+  //     res.status(500).send(error);
+  //     next();
+  //   });
+
+  // return (models.Sessions.get())
+
   //  If the cookie was found in the sessions table, assign that obj to the session (for each cookie)
-  //  assign object to cookies property on request (req._setCookiesVariable)
   //    if session assigned to user
   //    username and userID property assignment to session object
+  //  {userId: userId, cookie: cookie}
   //    else req._setSessionVariable with cookies?
   //  return 'cookies are present' so we can catch malicious cookies
   //if no cookies
